220132f14b8ad62f23d7ea940c0902a6d7731f0b
KRYSKO Kiryha <KKO8@dsone.3ds.com>
2023-06-05 17:37:24 -0400

    lesson flow

--- assignments/touch_type_workout/touch_type.py
+++ assignments/touch_type_workout/touch_type.py
@@ -4,6 +4,7 @@
 
 import os
 import time
+import json
 from PySide2 import QtWidgets, QtCore, QtGui
 from ui import ui_main
 
@@ -13,13 +14,142 @@ class TouchType(QtWidgets.QMainWindow, ui_main.Ui_TouchType):
         super(TouchType, self).__init__()
         self.setupUi(self)
 
+        # Data
+        self.keyboard_blank = f'{root}/data/images/blank.jpg'
+        self.lessons = f'{root}/data/lessons.json'
+        self.tests = f'{root}/data/tests.json'
+        self.lessons_data = None
+        self.tests_data = None
+
+        # Flow
+        self.lesson_started = False
+        self.lesson_name = None
+        self.sequence_text = None
+        self.number_of_sequences = 0
+        self.current_sequence = 0
+        self.current_string = 0
+        self.string_length = None
+
+        self.init_ui()
+
+        # UI signals
+        self.btnStartLesson.pressed.connect(self.start_lesson)
+
+    def reset_ui(self):
+
+        pixmap = QtGui.QPixmap(self.keyboard_blank)
+        self.labPictures.setPixmap(pixmap)
+        self.linTask.clear()
+
+    def init_ui(self):
+
+        # Load keyboard
+        pixmap = QtGui.QPixmap(self.keyboard_blank)
+        # self.labPictures.setPixmap(pixmap.scaled(self.label.size(), QtCore.Qt.IgnoreAspectRatio))
+        self.labPictures.setPixmap(pixmap)
+
+        # Load lessons and tests
+        with open(self.lessons, 'r') as file_content:
+            self.lessons_data = json.load(file_content)
+
+        with open(self.lessons, 'r') as file_content:
+            self.tests_data = json.load(file_content)
+
+        self.comLessons.addItems(self.lessons_data.keys())
+        self.comTests.addItems(self.tests_data.keys())
+
+    def init_sequence(self):
+
+        # lesson = self.comLessons.currentText()
+        self.sequence_text = self.lessons_data[self.lesson_name]['sequences'][self.current_sequence]
+        self.linTask.setText(self.sequence_text)
+        pixmap = f'{root}/data/images/{self.sequence_text[self.current_string].upper()}_blue.jpg'
+        self.labPictures.setPixmap(pixmap)
+
+        self.current_string = 1
+        self.string_length = len(self.sequence_text)
+        print(f'len = {self.string_length}')
+
+    def init_lesson(self):
+
+        self.sequence_text = self.lessons_data[self.lesson_name]['sequences'][self.current_sequence]
+        self.linTask.setText(self.sequence_text)
+        pixmap = f'{root}/data/images/{self.sequence_text[self.current_string].upper()}_blue.jpg'
+        self.labPictures.setPixmap(pixmap)
+
+        self.current_string = 1
+        self.string_length = len(self.sequence_text)
+        print(f'len = {self.string_length}')
+
+    def start_lesson(self):
+
+        self.lesson_name = self.comLessons.currentText()
+
+        self.number_of_sequences = len(self.lessons_data[self.lesson_name]['sequences'])
+        self.current_sequence = 0
+        self.current_string = 0
+        self.init_sequence()
+        self.lesson_started = True
+
+    def keyPressEvent(self, event):
+
+        # if event.key() == QtCore.Qt.Key_A:
+        #     keyboard_a = "C:/Users/kko8/OneDrive/projects/master/ECE554/assignments/project/src/pressed_a.jpg"
+        #     pixmap = QtGui.QPixmap(keyboard_a)
+        #     self.labPictures.setPixmap(pixmap)
+        #     print('A key was pressed')
+        #     self.trigger = 1
+        # else:
+        #     print(f'Key pressed: {event.key()}')
+
+        if self.lesson_started:
+            print('Lesson started')
+
+            print(f'current sequence = {self.current_sequence}')
+            # if self.number_of_sequences == self.current_sequence:
+            #     print('LAST')
+
+            print(f'current string = {self.current_string}')
+            if self.current_string == self.string_length:
+                if self.number_of_sequences == self.current_sequence + 1:
+                    print('LAST')
+                    self.lesson_started = False
+                    self.reset_ui()
+                    return
+
+                print('End Sequence')
+                self.current_string = 0
+                self.current_sequence += 1
+                self.init_sequence()
+                return
+
+            if self.sequence_text[self.current_string] == ' ':
+                key = 'space'
+            else:
+                key = self.sequence_text[self.current_string].upper()
+
+            pixmap = f'{root}/data/images/{key}_blue.jpg'
+            self.labPictures.setPixmap(pixmap)
+
+            self.current_string += 1
+
+    def keyReleaseEvent(self, event):
+        # # This method will be called every time a key is released
+        # if event.key() == QtGui.Qt.Key_A:
+        #     print('A key was released')
+        # else:
+        #     print(f'Key released: {event.key()}')
+
+        # pixmap = QtGui.QPixmap(self.keyboard_blank)
+        # self.labPictures.setPixmap(pixmap)
+        pass
+
 
 if __name__ == "__main__":
 
-    # root = os.path.dirname(os.path.abspath(__file__))
+    root = os.path.dirname(os.path.abspath(__file__))
     app = QtWidgets.QApplication([])
     split_smart = TouchType()
     # split_smart.setWindowIcon(QtGui.QIcon('{0}/icons/split_smart.ico'.format(root)))
     split_smart.show()
     app.exec_()
-    
--- assignments/touch_type_workout/ui/ui_main.py
+++ assignments/touch_type_workout/ui/ui_main.py
@@ -17,13 +17,71 @@ class Ui_TouchType(object):
     def setupUi(self, TouchType):
         if not TouchType.objectName():
             TouchType.setObjectName(u"TouchType")
-        TouchType.resize(1135, 587)
+        TouchType.resize(1079, 627)
         self.centralwidget = QWidget(TouchType)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab_1 = QWidget()
+        self.tab_1.setObjectName(u"tab_1")
+        self.verticalLayout_2 = QVBoxLayout(self.tab_1)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.splitter = QSplitter(self.tab_1)
+        self.splitter.setObjectName(u"splitter")
+        self.splitter.setOrientation(Qt.Horizontal)
+        self.label = QLabel(self.splitter)
+        self.label.setObjectName(u"label")
+        self.splitter.addWidget(self.label)
+        self.comLessons = QComboBox(self.splitter)
+        self.comLessons.setObjectName(u"comLessons")
+        self.splitter.addWidget(self.comLessons)
+        self.btnStartLesson = QPushButton(self.splitter)
+        self.btnStartLesson.setObjectName(u"btnStartLesson")
+        self.splitter.addWidget(self.btnStartLesson)
+        self.line = QFrame(self.splitter)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShape(QFrame.VLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+        self.splitter.addWidget(self.line)
+        self.label_2 = QLabel(self.splitter)
+        self.label_2.setObjectName(u"label_2")
+        self.splitter.addWidget(self.label_2)
+        self.comTests = QComboBox(self.splitter)
+        self.comTests.setObjectName(u"comTests")
+        self.splitter.addWidget(self.comTests)
+        self.btnStartTest = QPushButton(self.splitter)
+        self.btnStartTest.setObjectName(u"btnStartTest")
+        self.splitter.addWidget(self.btnStartTest)
+
+        self.verticalLayout_2.addWidget(self.splitter)
+
+        self.linTask = QLineEdit(self.tab_1)
+        self.linTask.setObjectName(u"linTask")
+
+        self.verticalLayout_2.addWidget(self.linTask)
+
+        self.labPictures = QLabel(self.tab_1)
+        self.labPictures.setObjectName(u"labPictures")
+
+        self.verticalLayout_2.addWidget(self.labPictures)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
+        self.tabWidget.addTab(self.tab_1, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.verticalLayout.addWidget(self.tabWidget)
+
         TouchType.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(TouchType)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1135, 21))
+        self.menubar.setGeometry(QRect(0, 0, 1079, 21))
         TouchType.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(TouchType)
         self.statusbar.setObjectName(u"statusbar")
@@ -31,10 +89,20 @@ class Ui_TouchType(object):
 
         self.retranslateUi(TouchType)
 
+        self.tabWidget.setCurrentIndex(0)
+
+
         QMetaObject.connectSlotsByName(TouchType)
     # setupUi
 
     def retranslateUi(self, TouchType):
         TouchType.setWindowTitle(QCoreApplication.translate("TouchType", u"Touch Type Workout", None))
+        self.label.setText(QCoreApplication.translate("TouchType", u"Lessons: ", None))
+        self.btnStartLesson.setText(QCoreApplication.translate("TouchType", u"Start Lesson", None))
+        self.label_2.setText(QCoreApplication.translate("TouchType", u"Tests: ", None))
+        self.btnStartTest.setText(QCoreApplication.translate("TouchType", u"Start Test", None))
+        self.labPictures.setText(QCoreApplication.translate("TouchType", u"PICTURES", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_1), QCoreApplication.translate("TouchType", u"Lessons and Tests", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("TouchType", u"Statistics", None))
     # retranslateUi
 
